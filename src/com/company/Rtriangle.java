af6487251f2f06159a2efde0831f96d52c7bed59
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-08-21 17:54:58 +0300

    add interface

---
+++ src/com/company/Rtriangle.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * Created by dmitry on 21/08/15.
+ */
+public interface Rtriangle {
+
+    int getApexX1();
+
+    int getApexY1();
+
+    int getApexX2();
+
+    int getApexY2();
+
+    int getApexX3();
+
+    int getApexY3();
+
+
+}
---
+++ src/com/company/RtriangleProvider.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by dmitry on 21/08/15.
+ */
+public final class RtriangleProvider {
+
+    public static Rtriangle getRtriangle() {
+    }
+
+}
+
